2a95af274c005f29ff174a47c35a85d94cbe69f5
Sergio-Dzu <sergei.dzubak@gmail.com>
2021-10-02 15:18:01 +0300

    Fix up slick slider

---
+++ js/slick-main.js
@@ -0,0 +1,10 @@
+$('.single-item').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    
+  });
+
--- js/slick-slaider.js
+++
@@ -1,6 +0,0 @@
-$('.mc-slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
