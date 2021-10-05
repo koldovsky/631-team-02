999b43a0bfd5c0d9989d0111450fc42315b51eee
zavhorodnia-nastia <zavgorodnia12345@gmail.com>
2021-10-05 12:47:14 +0000

    Adfooter form validation

---
+++ js/footer-form.js
@@ -0,0 +1,49 @@
+const form = document.getElementById('form_email');
+const email = document.getElementById('email_input');
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
+
+
+function checkRequired(inputArr) {
+    let isRequired = false;
+    inputArr.forEach(function(input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+            isRequired = true;
+        } else {
+            showSuccess(input);
+        }
+    });
+
+    return isRequired;
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (!checkRequired([email])) {
+        checkEmail(email);
+    }
+});
