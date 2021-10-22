8bc1c6a7e88d390fd6ed6d3478a8681cdf756b4d
unknown <vuntaj@gmail.com>
2021-10-22 13:30:21 +0300

    href

--- js/shop_instruments.js
+++ js/shop_instruments.js
@@ -1,7 +1,7 @@
 function show4Instruments(groups) {
   const instrumentContainer = document.querySelector(".selection_instruments");
   instrumentContainer.innerHTML = "";
-  for (const elem of groups) {
+  for (let elem of groups) {
     instrumentContainer.innerHTML += `<div>
       <div>
         <a class="toProduct_${elem.id}"  href="page_product_image.html" onmouseover="localStorage.setItem('id', '${elem.id}')">
@@ -24,7 +24,7 @@ function show4Instruments(groups) {
 async function load4Instruments() {
   const instruments = await instrumentsS.getInstruments();
   document
-    .querySelector(".shop_button_section .button_eg_guitar")
+    .querySelector(".button_eg_guitar")
     .addEventListener("click", () => show4Instruments(instruments.slice(0, 4)));
   document
     .querySelector(".button_ag_guitar")
